789978648c100ae2aca3b6796be7a93a555abb65
gegorov <gegorov@yahoo.com>
2020-05-21 23:20:38 +0300

    Refactor App to use Context

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import About from './pages/About';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
+import AlertState from './context/alert/AlertState';
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <div className="container pt-4">
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/profile/:name" component={Profile} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <AlertState>
+      <BrowserRouter>
+        <Navbar />
+        <div className="container pt-4">
+          <Alert alert={{ text: 'test-alert' }} />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/profile/:name" component={Profile} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </AlertState>
   );
 }
 
