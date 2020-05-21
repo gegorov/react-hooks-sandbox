df6ee8b3b2b2c0cace06e5f40790b3add3e61b39
gegorov <gegorov@yahoo.com>
2020-05-21 23:19:26 +0300

    Create Alert component

---
+++ src/components/Alert.js
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+import { AlertContext } from '../context/alert/alertContext';
+
+const Alert = () => {
+  const { alert, hide } = useContext(AlertContext);
+
+  if (!alert) {
+    return null;
+  }
+  const { text, type } = alert;
+
+  return (
+    <div
+      className={`alert alert-${type || 'secondary'} alert-dismissible fade show`}
+      role="alert"
+    >
+      {text}
+      <button type="button" className="close" aria-label="Close" onClick={hide}>
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+  );
+};
+
+export default Alert;
