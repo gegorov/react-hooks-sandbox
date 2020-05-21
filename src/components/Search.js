c79df3e52b8026a29d3d4e37891dbffc39077755
gegorov <gegorov@yahoo.com>
2020-05-22 01:15:36 +0300

    Add githubState, context and reducer

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,12 @@
 import React, { useContext, useState } from 'react';
 import { AlertContext } from '../context/alert/alertContext';
+import GithubContext from '../context/github/githubContext';
 
 
 const Search = () => {
   const { show } = useContext(AlertContext);
+  const github = useContext(GithubContext);
+
   const [value, setValue] = useState('');
   const onSubmitHandler = (event) => {
     if (event.key !== 'Enter') {
@@ -12,6 +15,7 @@ const Search = () => {
     if (value.trim()) {
       // get data
       console.log(`Make request with: ${value}`);
+      github.search(value.trim());
     } else {
       show('Please enter correct value');
     }
