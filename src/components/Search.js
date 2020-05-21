565ea4d8fcc71722d3134f6d5bf9ffbf7621278e
gegorov <gegorov@yahoo.com>
2020-05-21 23:55:12 +0300

    Refactor Search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,19 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AlertContext } from '../context/alert/alertContext';
 
 
 const Search = () => {
   const { show } = useContext(AlertContext);
+  const [value, setValue] = useState('');
   const onSubmitHandler = (event) => {
-    if (event.key === 'Enter') {
-      show('this is alert', 'warning');
+    if (event.key !== 'Enter') {
+      return;
+    }
+    if (value.trim()) {
+      // get data
+      console.log(`Make request with: ${value}`);
+    } else {
+      show('Please enter correct value');
     }
   };
   return (
@@ -16,6 +23,8 @@ const Search = () => {
         className="form-control"
         placeholder="Please enter Github name"
         onKeyPress={onSubmitHandler}
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
       />
     </div>
   );
