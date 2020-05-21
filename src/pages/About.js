cf19af33a8674f15d21856667a7b7c2f572a3d1b
gegorov <gegorov@yahoo.com>
2020-05-21 20:34:45 +0300

    Add basic components

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => (
+  <div className="jumbotron jumbotron-fluid">
+    <div className="container">
+      <h1 className="display-4">Info</h1>
+      <p className="lead">kfjs;glkfg;slkjgs;lkdjsd</p>
+    </div>
+  </div>
+);
+
+export default About;
